98ba85319928cb05612de3c9b02d1279c2afd6dc
Dmytro Sherstiuk <sherdg@gmail.com>
2022-09-05 21:19:47 +0300

    JS

---
+++ JavaScript/test.js
@@ -0,0 +1,80 @@
+// var test = 'апельсин';
+// var test1 = 42;
+// var test = false;
+
+// var test = {                    // Object - ключі як правило пишуть латинськими літерами
+//     Name: "Микита",          // Значення можуть бути любого з типу з 8 відомих
+//     Age: 25,
+//     Child: {
+//         Name: "Софія",
+//         Age: 4
+//     }
+// };
+
+// console.log(Object.keys(test));
+
+// var test = [ 'Name', 'Age', 'Child' ];
+// // console.log(test[1]);
+// console.log(test.length);
+
+
+// var f = null;                // Null
+// console.log(f);
+
+// var g = undefined;           // Undefined
+// var g1
+// console.log(g1+1);
+
+
+const MY_FAV = 7;
+// // данное присваивание выдаст ошибку - Uncaught TypeError: Assignment to constant variable.
+// MY_FAV = 20;
+
+// // напечатает 7
+// console.log("my favorite number is: " + MY_FAV);
+
+
+
+
+// if(MY_FAV===7){
+//     var randomNumber = Math.floor(Math.random() * 100);
+//     let test;
+// }
+// else{
+//     var randomNumber = Math.floor(Math.random() * 50);
+//     let test;
+// };
+// var randomNumber1 = Math.floor(Math.random() * 100);
+// console.log(randomNumber1);
+
+// for(let i =0; i++; i<10){
+
+// };
+
+// function test(){
+//     let myVar = 'test';
+//    }
+
+;
+
+// const name = 'Chris';
+// const greeting = `Hello, ${name}`;
+// // console.log('Name ->'+" Age");
+// console.log(greeting); // "Hello, Chris"
+
+
+// const browserType = 'mozilla5';
+// browserType.length;
+// console.log(browserType.length);
+// console.log(browserType[1]);
+// console.log(browserType[browserType.length - 1]);
+
+
+// const browserType = 'mozilla';
+const browserType = 'chrome';
+
+if (browserType.includes('lla')) {
+  console.log('Found lla!');
+} else {
+  console.log('No zilla here!');
+};
